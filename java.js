5c535d24ad80e30b4b896de76afc3a6fdcd24116
Yasin <yasinmass3069@gmail.com>
2024-08-17 00:35:11 +0530

    kgisl

---
+++ java.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "220px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
